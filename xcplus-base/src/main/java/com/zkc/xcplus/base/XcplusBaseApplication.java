c96cbffa5cda75ae9a01161396831c2565ce5d8d
kcTest <763336180@qq.com>
2023-02-20 17:39:36 +0800

    Initial commit

---
+++ xcplus-base/src/main/java/com/zkc/xcplus/base/XcplusBaseApplication.java
@@ -0,0 +1,13 @@
+package com.zkc.xcplus.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcplusBaseApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(XcplusBaseApplication.class, args);
+	}
+	
+}
