28d5aee68c9fb8ed2f75e6df26dc4c1615edee65
kcTest <763336180@qq.com>
2023-02-24 16:00:35 +0800

    跨域请求设置

--- xcplus-system/xcplus-system-api/src/main/java/com/zkc/xcplus/system/api/config/GlobalCorsConfig.java
+++ xcplus-system/xcplus-system-api/src/main/java/com/zkc/xcplus/system/api/config/GlobalCorsConfig.java
@@ -1,4 +1,30 @@
 package com.zkc.xcplus.system.api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域请求设置
+ */
+@Configuration
 public class GlobalCorsConfig {
+	
+	@Bean
+	public CorsFilter getCorsFilter() {
+		
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedHeader("*");
+		//允许携带cookie
+		configuration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource configurationSourceSource = new UrlBasedCorsConfigurationSource();
+		configurationSourceSource.registerCorsConfiguration("/**", configuration);
+		
+		return new CorsFilter(configurationSourceSource);
+	}
 }
