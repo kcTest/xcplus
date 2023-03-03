1183e8df167bb1cb9d8eb1f1b23bc95562959f82
kcTest <763336180@qq.com>
2023-03-03 21:25:05 +0800

    nacos 服务注册

--- xcplus-system/xcplus-system-api/src/main/java/com/zkc/xcplus/system/api/SystemApiApplication.java
+++ xcplus-system/xcplus-system-api/src/main/java/com/zkc/xcplus/system/api/SystemApiApplication.java
@@ -2,6 +2,7 @@ package com.zkc.xcplus.system.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.zkc.xcplus")
